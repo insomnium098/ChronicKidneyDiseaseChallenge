fd378487a557f1a016c54fc8965a3e130e335085
insomnium098 <insomnium098@gmail.com>
2021-07-17 17:08:09 -0500

    Finished the meds, although it lacks the drug clinical considerations

--- 2_DataPreparation/Script_DataPreparation.R
+++ 2_DataPreparation/Script_DataPreparation.R
@@ -2,6 +2,7 @@
 ###Chronic Kidney Disease Challenge
 
 library(dplyr)
+library(fastDummies)
 
 response <- read.csv("../1_OriginalDatasets/T_stage.csv")
 dbp <- read.csv("../1_OriginalDatasets/T_DBP.csv")
@@ -48,19 +49,27 @@ for (i in unique(meds$id)){
                           df_filt$end_day > counter
                       ,]
     
+    
     ##Case when the patient has no data in the window
     if(nrow(df_filt2) == 0){
       counter <- counter + window
       next
     }
     
+    ###In this part, we need to summarize the meds by patient
+    
     ##We add the number of drugs taken in the same window
     df_filt2$WindowNumberDrugs <- length(unique(df_filt2$drug))
-    df_filt2$WindowCombinationDrugs <- paste(unique(df_filt2$drug), collapse = "-")
-    ###We add the combinationof drugs
+    ###We add the combination of drugs,  the dosage and  the durationDose per med 
+    df_filt2$WindowCombinationDrugs <-paste(c((df_filt2$drug), 
+                                              (df_filt2$daily_dosage)), collapse = "-")
     
     
-    df_filt2$Window <- paste0(counter ,"-" ,counter + window)
+    
+    
+    
+    
+    df_filt2$Window <- counter + window#paste0(counter ,"-" ,counter + window)
     
     if(!exists("df_final")){
       df_final <- df_filt2
@@ -73,17 +82,60 @@ for (i in unique(meds$id)){
   
   
   
-  if(!exists("final")){
-    final <- df_final
+  if(!exists("meds_window")){
+    meds_window <- df_final
   } else{
-    final <- rbind(final, df_final)
+    meds_window <- rbind(meds_window, df_final)
   }
   
   rm(df_final)
   
 
   
+}
+
+rm(df_filt, df_filt2)
+
+###One approach was to summarize the meds by patient and window,
+### however by doing dummys we were losing various continous and
+### important variables such as the dose. The second reason to do
+### this was to obtain more data to train the model. By doing this
+## we were obtaining 7800 rows, in contrast to +- 3600 of the 
+## summarization method.
+
+
+###Now that we have the meds by window, we calculate
+###The sum of the dosage per day by window
+
+
+for (patient in unique(meds_window$id)){
+  ###First we filter by patient
+  df_patient <- filter(meds_window, id == patient)
+  
+  for (patientWindow in unique(df_patient$Window)){
+    df_patient_window <- filter(df_patient, Window == patientWindow)
+    df_patient_window$sumDailyDosagePerWindow <- sum(df_patient_window$daily_dosage)
+    
+    if(!exists("meds_final")){
+      meds_final <- df_patient_window
+    } else {
+      meds_final <- rbind(meds_final,df_patient_window)
+    }
+    
   }
+  
+  
+  
+}
+rm(df_patient, df_patient_window, meds_window)
+
+
+###TODO : RESEARCH EACH DRUG, ADDING clinical information for every
+###drug
+
+
+
+
 
 
 
